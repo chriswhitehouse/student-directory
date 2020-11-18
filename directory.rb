084c980cd3459849ae4f6df6e020ee11f71e7380
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-11-18 10:11:30 +0000

    Step 14, Ex3refactpring the code further

--- directory.rb
+++ directory.rb
@@ -1,88 +1,94 @@
-# create an empty array
 @students = []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  get_name
+end
 
-  # get the first name
-  name = STDIN.gets.delete("\n")
-  # while the name is not empty, repeat this code
+def get_name
+  name = STDIN.gets.chomp
   while !name.empty? do
-    # get cohort
-    puts "Cohort: "
-    cohort = STDIN.gets.chomp.to_sym
-    if cohort.empty? then
-      cohort = "november"
-    end
-    # get hobby
-    #puts "Hobby: "
-    #hobby = gets.chomp
-
-    # add the student hash to the array
-    add_student(name, cohort)
+    add_student(name, get_cohort)
     puts "Now we have #{@students.count} students"
-    # get another name from the user
     name = STDIN.gets.chomp
   end
-  # return the array of students
-  @students
 end
+
+def get_cohort
+  puts "Cohort: "
+  cohort = STDIN.gets.chomp.to_sym
+  if cohort.empty? then
+    cohort = "november"
+  end
+  return cohort
+end
+
 
 def print_header
   puts   "The students of Villains Academy".center(60)
   puts "-------------".center(60)
 end
-# somthing
+
+
 def print_students_list
-  # find distinct cohorts
   if @students.empty? then
     return
   else
-    cohorts = []
-    @students.map { |student| if !cohorts.include?(student[:cohort]) then cohorts << student[:cohort] end }
-
-      # iterate over cohorts array
-      cohorts.each do |cohort|
-        puts ""
-        puts cohort.to_s.center(60)
-        count = 0
-        # while !students[count].nil? do
-        # if students[count][:name][0].downcase == "c" && students[count][:name].length < 12 then
-        @students.each do |student|
-          if cohort == student[:cohort]
-            puts "#{count+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(60)
-            count += 1
-          end
-        end
-      end
-    end
+    print_cohorts
+  end
 end
+
+
+def cohort_list
+  cohorts = []
+  @students.map { |student| if !cohorts.include?(student[:cohort]) then cohorts << student[:cohort] end }
+  return cohorts
+end
+
+
+def print_cohorts
+  cohort_list.each do |cohort|
+      puts ""
+      puts cohort.to_s.center(60)
+      print_students(cohort)
+  end
+end
+
+
+def print_students(cohort)
+  count = 0
+  @students.each do |student|
+    if cohort == student[:cohort]
+      puts "#{count+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(60)
+      count += 1
+    end
+  end
+end
+
 
 def print_footer
   @students.length > 1 ? plural_string = "s" : plural_string = ""
   puts "Overall, we have #{@students.count} great student#{plural_string}".center(60)
+  puts ""
 end
+
 
 def interactive_menu
   loop do
-    # 1. print the menu and ask the user what to do
-    # 2. read the input and save it into a variable
-
     print_menu
     process(STDIN.gets.chomp)
-    # 3. do what the user has asked
   end
 end
 
+
 def print_menu
-  # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
-  puts "4. Load the lost from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
-  # 2. read the input and save it into a variable
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
 end
+
 
 def show_students
   print_header
@@ -90,29 +96,27 @@ def show_students
   print_footer
 end
 
+
 def process(selection)
   case selection
     when "1"
-      # input the students
-      @students = input_students
+      input_students
     when "2"
-      # show the students
       show_students
     when "3"
       save_students
     when "4"
       load_students
     when "9"
-      exit # this will casue the program to terminate
+      exit
     else
       puts "I don't know what you meant, try again"
   end
 end
 
+
 def save_students
-  # open file to writing
   file = File.open("students.csv", "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -120,6 +124,7 @@ def save_students
   end
   file.close
 end
+
 
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
@@ -131,12 +136,13 @@ def load_students(filename = "students.csv")
   puts "Loaded #{@students.count} from #{filename}"
 end
 
+
 def try_load_students
-  filename = ARGV.first # first argument from the command line
+  filename = ARGV.first
   if filename.nil? then
     load_students
-  else # get out of the method if it isn't given
-    if File.exists?(filename) # if it exists
+  else
+    if File.exists?(filename)
       load_students(filename)
       puts "Loaded #{@students.count} from #{filename}"
     else # if it doesn't exist
@@ -145,6 +151,7 @@ def try_load_students
     end
   end
 end
+
 
 def add_student(name, cohort)
   @students << {name: name, cohort: cohort.to_sym}
